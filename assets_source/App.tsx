0a8fd2881eee6581f2be2c5361f54d5f891768a7
nwrose <nwrose@umich.edu>
2024-06-08 13:55:41 -0400

    added frontend-backend api starter

--- assets_source/App.tsx
+++ assets_source/App.tsx
@@ -7,6 +7,37 @@ import './App.css'
 
 
 const App: React.FC = () => {
+  const ENDPOINT_URL = "";
+
+  interface BackendResponse {
+    success: boolean;
+    message: Record<string, unknown>;
+  }
+
+  const requestFromBackend = async (jsonData: Record<string, unknown>) : Promise<BackendResponse> => {
+    try {
+      const msg = {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          //other headers
+        },
+        body: JSON.stringify(jsonData),
+      }
+      const response = await fetch(ENDPOINT_URL, msg);
+      if (!response.ok){
+        throw new Error('Failed to send data to the backend');
+      }
+
+      // handle response
+      const responseData = await response.json();
+      return responseData as BackendResponse;
+
+    } catch (error) {
+      alert("error sending data to backend");
+      return { success: false, message: {} };
+    }
+  };
 
   const [showRose, setShowRose] = useState(false);
   const [inputsJSON, setInputsJSON] = useState({})
@@ -14,7 +45,8 @@ const App: React.FC = () => {
   const handleShowRose = (updatedJSON : Record<string, unknown>) => {
     setInputsJSON(updatedJSON);
     // GET ROSE IMG FROM BACKEND //
-
+    const response = requestFromBackend(inputsJSON);
+    
     ///////////////////////////////
     setShowRose(true);
   };
