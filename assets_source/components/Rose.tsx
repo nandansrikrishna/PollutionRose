be2bf50249b9d595d57202997d2defc20d210ee1
nwrose <nwrose@umich.edu>
2024-06-11 17:01:36 -0400

    styling adjustments

--- assets_source/components/Rose.tsx
+++ assets_source/components/Rose.tsx
@@ -1,16 +1,9 @@
 import React, { } from 'react'
 
 const Rose: React.FC<{inputsJSON: Record<string, unknown>}> = ({inputsJSON}) => {
-  const reset = () => window.location.reload()
     return (
       <>
-      <p></p>
-      <div> 
-        <button onClick={reset}>Reset</button>
- 
-      </div>
       <div id="chartDiv" className="rose">
-        <p>rose img</p>
         {JSON.stringify(inputsJSON)}
       </div>
       </>
